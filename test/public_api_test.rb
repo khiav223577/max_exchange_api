6436a49ba50c5481d46aba767f8eda749bab00ab
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2022-07-22 13:53:47 +0800

    Update test cases

--- test/public_api_test.rb
+++ test/public_api_test.rb
@@ -46,9 +46,32 @@ class PublicApiTest < Minitest::Test
     end
 
     assert_instance_of(Array, response)
-    assert_equal({ 'id' => 'twd', 'precision' => 0, 'sygna_supported' => false }, response[0])
-    assert_equal({ 'id' => 'btc', 'precision' => 8, 'sygna_supported' => true }, response[1])
-    assert_equal({ 'id' => 'eth', 'precision' => 8, 'sygna_supported' => true }, response[2])
+    assert_equal(
+      {
+        'id'                 => 'twd',
+        'precision'          => 0,
+        'sygna_supported'    => false,
+        'm_wallet_supported' => true,
+        'min_borrow_amount'  => '',
+      }, response[0])
+
+    assert_equal(
+      {
+        'id'                 => 'btc',
+        'precision'          => 8,
+        'sygna_supported'    => true,
+        'm_wallet_supported' => true,
+        'min_borrow_amount'  => '0.001',
+      }, response[1])
+
+    assert_equal(
+      {
+        'id'                 => 'eth',
+        'precision'          => 8,
+        'sygna_supported'    => true,
+        'm_wallet_supported' => true,
+        'min_borrow_amount'  => '0.01',
+      }, response[2])
   end
 
   def test_k_api
@@ -94,24 +117,28 @@ class PublicApiTest < Minitest::Test
     expected1 = {
       'id'                   => 'maxtwd',
       'name'                 => 'MAX/TWD',
+      'market_status'        => 'active',
       'base_unit'            => 'max',
       'base_unit_precision'  => 2,
       'min_base_amount'      => 21.0,
       'quote_unit'           => 'twd',
       'quote_unit_precision' => 4,
       'min_quote_amount'     => 250.0,
+      'm_wallet_supported'   => false,
     }
     assert_equal(expected1, response[0])
 
     expected2 = {
       'id'                   => 'btctwd',
       'name'                 => 'BTC/TWD',
+      'market_status'        => 'active',
       'base_unit'            => 'btc',
       'base_unit_precision'  => 8,
-      'min_base_amount'      => 0.00015,
+      'min_base_amount'      => 0.0004,
       'quote_unit'           => 'twd',
       'quote_unit_precision' => 1,
       'min_quote_amount'     => 250.0,
+      'm_wallet_supported'   => true,
     }
     assert_equal(expected2, response[1])
   end
@@ -123,8 +150,8 @@ class PublicApiTest < Minitest::Test
 
     assert_instance_of(Hash, response)
     assert_instance_of(Hash, response['tickers'])
-    assert_equal(%w[at buy sell open low high last volume volume_in_btc], response['tickers']['btcusdt'].keys)
-    assert_equal(%w[at buy sell open low high last volume volume_in_btc], response['tickers']['maxtwd'].keys)
+    assert_equal(%w[at buy high last low open sell volume volume_in_btc], response['tickers']['btcusdt'].keys.sort)
+    assert_equal(%w[at buy high last low open sell volume volume_in_btc], response['tickers']['maxtwd'].keys.sort)
   end
 
   def test_tickers_api
